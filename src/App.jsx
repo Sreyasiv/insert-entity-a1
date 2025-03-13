61bfd6003318726097ddbcd89383cac539e1d8b4
Sreya Sivakumar <sreyas@Sreyas-MacBook-Air.local>
2025-03-13 09:42:43 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import BookCard from "./components/BookCard";
-import Home from "./components/Home";
-import AddBook from "./components/AddBook/AddBook";
-import books from "./booksData";
+import { useState } from "react";
+import Home from "./Home";
+import AddBook from "./components/AddBook";
+import booksData from "./booksData";
 import "./Home.css";
 
 function App() {
+  const [books, setBooks] = useState(booksData);
+
+  const addNewBook = (newBook) => {
+    setBooks([...books, { id: books.length + 1, ...newBook }]);
+  };
+
   return (
     <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/add-book">Add Book</Link>
+      </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/add-book" element={<AddBook />} />
+        <Route path="/" element={<Home books={books} />} />
+        <Route path="/add-book" element={<AddBook addNewBook={addNewBook} />} />
       </Routes>
     </Router>
   );
